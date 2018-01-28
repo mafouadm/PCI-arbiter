8be724de14267bad5b9c347bfe0a2962cb14b999
mohamed fouad <33027971+mafouadm@users.noreply.github.com>
2018-01-28 02:10:00 +0200

    Add files via upload

--- .gitignore/arbiter.v
+++ .gitignore/arbiter.v
@@ -4,160 +4,222 @@ input clk;
 input GLOBAL_IRDY,GLOBAL_TRDY,GLOBAL_FRAME;
 input  [7:0] REQ;
 output reg [7:0] GNT;
-wire [7:0] dataOut;
-reg wr, rd;
-reg [7:0] datain;
-reg en , rst;
-wire EMPTY , FULL;
+
+wire [7:0] data;
+integer wrcount=0;
+integer rdcount=0;
+reg [7:0] fifo [0:31];
 parameter a=8'b00000001, b=8'b00000010, c=8'b00000100, d=8'b00001000, e=8'b00010000, f=8'b00100000, g=8'b01000000, h=8'b10000000;
   reg[7:0] prevRequest; 
+reg [7:0] dataIn;
+ wire [7:0] dataOut ;
 always @(posedge clk)begin
   if(REQ!==prevRequest)
 begin
-prevRequest<=REQ;
-if(REQ[0]==0)
-begin
-wr<=1;
-rd<=0;
-datain<=a;
 
-	
+if((REQ[0]==0)&&(REQ[0]!=prevRequest[0]))
+begin
+fifo[wrcount]<=a;
+wrcount=wrcount+1;
+prevRequest[0]<=REQ[0];
 end
  
- if(REQ[1]==0)
+ if((REQ[1]==0)&&(REQ[1]!=prevRequest[1]))
 begin
-wr<=1;
-rd<=0;
-datain<=b;
+fifo[wrcount]<=b;
+wrcount=wrcount+1;
+prevRequest[1]<=REQ[1];
 	
 end
- if(REQ[2]==0)
+ if((REQ[2]==0)&&(REQ[2]!=prevRequest[2]))
 begin
-wr<=1;
-rd<=0;
-datain<=c;
+
+fifo[wrcount]<=c;
+wrcount=wrcount+1;
+	prevRequest[2]<=REQ[2];
+end
+ if((REQ[3]==0)&&(REQ[3]!=prevRequest[3]))
+begin
+fifo[wrcount]<=d;
+wrcount=wrcount+1;
+prevRequest[3]<=REQ[3];
 	
 end
- if(REQ[3]==0)
+ if((REQ[4]==0)&&(REQ[4]!=prevRequest[4]))
 begin
-wr<=1;
-rd<=0;
-datain<=d;
+fifo[wrcount]<=e;
+wrcount=wrcount+1;
+prevRequest[4]<=REQ[4];
 	
 end
- if(REQ[4]==0)
+ else if((REQ[5]==0)&&(REQ[5]!=prevRequest[5]))
 begin
-wr<=1;
-rd<=0;
-datain<=e;
+fifo[wrcount]<=f;
+wrcount=wrcount+1;
+prevRequest[5]<=REQ[5];
 	
 end
- if(REQ[5]==0)
+ else if((REQ[6]==0)&&(REQ[6]!=prevRequest[6]))
 begin
-wr<=1;
-rd<=0;
-datain<=f;
+fifo[wrcount]<=g;
+wrcount=wrcount+1;
+prevRequest[6]<=REQ[6];
 	
 end
- if(REQ[6]==0)
+ else if((REQ[7]==0)&&(REQ[7]!=prevRequest[7]))
 begin
-wr<=1;
-rd<=0;
-datain<=g;
+fifo[wrcount]<=h;
+wrcount=wrcount+1;
+prevRequest[7]<=REQ[7];
 	
 end
- if(REQ[7]==0)
-begin
-wr<=1;
-rd<=0;
-datain<=h;
-	
+else
+prevRequest<=REQ;
 end
-end
+
 else 
 begin
 
-wr<=0;
-rd<=1;
-if(dataOut==a)
+if(fifo[rdcount]==a)
 begin
-if(GLOBAL_FRAME)
-GNT<=11111110;
+if(GLOBAL_FRAME)begin
+GNT[0]<=0;
+GNT[1]<=1;
+GNT[2]<=1;
+GNT[3]<=1;
+GNT[4]<=1;
+GNT[5]<=1;
+GNT[6]<=1;
+GNT[7]<=1;
+rdcount=rdcount+1;
+end
+end
+else if(fifo[rdcount]==b)
+begin
+ if(GLOBAL_FRAME)begin
+GNT[1]<=0;
+
+GNT[0]<=1;
+GNT[2]<=1;
+GNT[3]<=1;
+GNT[4]<=1;
+GNT[5]<=1;
+GNT[6]<=1;
+GNT[7]<=1;
+rdcount=rdcount+1;
+end
+end
+else if(fifo[rdcount]==c)
+begin
+ if(GLOBAL_FRAME)begin
+GNT[2]<=0;
+GNT[1]<=1;
+
+GNT[0]<=1;
+
+GNT[3]<=1;
+GNT[4]<=1;
+GNT[5]<=1;
+GNT[6]<=1;
+GNT[7]<=1;
+rdcount=rdcount+1;
+end
 end
 
- if(dataOut==b)
+else if(fifo[rdcount]==d)
 begin
-if(GLOBAL_FRAME)
-GNT<=11111101;
+ if(GLOBAL_FRAME)begin
+GNT[3]<=0;
+GNT[1]<=1;
+
+GNT[0]<=1;
+GNT[2]<=1;
+GNT[4]<=1;
+GNT[5]<=1;
+GNT[6]<=1;
+GNT[7]<=1;
+rdcount=rdcount+1;
 end
- if(dataOut==c)
-begin
-if(GLOBAL_FRAME)
-GNT<=11111011;
 end
-
- if(dataOut==d)
+ else if(fifo[rdcount]==e)
 begin
-if(GLOBAL_FRAME)
-GNT<=11110111;
+if(GLOBAL_FRAME)begin
+GNT[4]<=0;
+GNT[1]<=1;
+
+GNT[0]<=1;
+GNT[2]<=1;
+GNT[3]<=1;
+
+GNT[5]<=1;
+GNT[6]<=1;
+GNT[7]<=1;
+rdcount=rdcount+1;
 end
- if(dataOut==e)
-begin
-if(GLOBAL_FRAME)
-GNT<=11101111;
-end
-
-
-  if(dataOut==f)
-begin
-if(GLOBAL_FRAME)
-GNT<=11011111;
-end
-
-
-
- if(dataOut==g)
-begin
-if(GLOBAL_FRAME)
-GNT<=10111111;
 end
 
 
-  if(dataOut==h)
+ else if(fifo[rdcount]==f)
 begin
-if(GLOBAL_FRAME)
-GNT<=01111111;
+if(GLOBAL_FRAME)begin
+GNT[5]<=0;
+GNT[1]<=1;
+
+GNT[0]<=1;
+GNT[2]<=1;
+GNT[3]<=1;
+GNT[4]<=1;
+GNT[6]<=1;
+GNT[7]<=1;
+rdcount=rdcount+1;
+end
 end
 
 
+
+else if(fifo[rdcount]==g)
+begin
+if(GLOBAL_FRAME)begin
+GNT[6]<=0;
+GNT[1]<=1;
+
+GNT[0]<=1;
+GNT[2]<=1;
+GNT[3]<=1;
+GNT[4]<=1;
+GNT[5]<=1;
+GNT[7]<=1;
+rdcount=rdcount+1;
+end
+end
+
+
+else if(fifo[rdcount]==h)
+begin
+if(GLOBAL_FRAME)begin
+GNT[7]<=0;
+GNT[1]<=1;
+GNT[0]<=1;
+GNT[2]<=1;
+GNT[3]<=1;
+GNT[4]<=1;
+GNT[5]<=1;
+GNT[6]<=1;
+rdcount=rdcount+1;
+end
+end
+else 
+GNT<=8'b11111111;
 end
 end
 initial 
   begin
-
+	prevRequest='hff;
     GNT='hff;
- datain  = 8'h0;
 
-  rd  = 1'b0;
-
-  wr  = 1'b0;
-
-  en  = 1'b0;
-
-  rst  = 1'b1;
-
-  // Wait 100 ns for global reset to finish
-
-  #10;        
-
-  // Add stimulus here
-
-  en  = 1'b1;
-
-  rst  = 1'b1;
   end
 
-FIFObuffer f1( clk,datain,rd,wr,1,dataOut,0,EMPTY,FULL);
+
 
 
   
@@ -165,83 +227,3 @@ endmodule
 
 
 
-module FIFObuffer( Clk,dataIn,RD, WR,EN, dataOut, Rst,EMPTY,  FULL   ); 
-
-input  Clk, RD,WR,  EN,Rst;
-
-output  EMPTY, FULL;
-
-input   [7:0]    dataIn;
-
-output reg [7:0] dataOut; // internal registers 
-
-reg [2:0]  Count = 0; 
-
-reg [7:0] FIFO [0:7]; 
-
-reg [2:0]  readCounter = 0,writeCounter = 0; 
-
-assign EMPTY = (Count==0)? 1'b1:1'b0; 
-
-assign FULL = (Count==8)? 1'b1:1'b0; 
-
-always @ (posedge Clk) 
-
-begin 
-
- if (EN==0); 
-
- else begin 
-
-  if (Rst) begin 
-
-   readCounter = 0; 
-
-   writeCounter = 0; 
-
-  end 
-
-   else if (RD ==1'b1 && Count!=0 && WR==0) begin 
-
-   dataOut  = FIFO[readCounter]; 
-
-   readCounter = readCounter+1; 
-
-  end 
-
-  else if (WR==1'b1 && Count<8) begin
-   FIFO[writeCounter]  = dataIn; 
-
-   writeCounter  = writeCounter+1; 
-
-  end 
-
-  else; 
-
- end 
-
- if (writeCounter==8) 
-
-  writeCounter=0; 
-
- else if (readCounter==8) 
-
-  readCounter=0; 
-
- else;
-
- if (readCounter > writeCounter) begin 
-
-  Count=readCounter-writeCounter; 
-
- end 
-
- else if (writeCounter > readCounter) 
-
-  Count=writeCounter-readCounter; 
-
- else;
-
-end 
-
-endmodule
